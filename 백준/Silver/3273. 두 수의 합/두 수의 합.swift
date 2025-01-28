e2d2512c021843a011b4291831b07c01be767a46
Hano <88874280+Glsme@users.noreply.github.com>
2025-01-28 16:20:26 +0900

    [Silver III] Title: 두 수의 합, Time: 48 ms, Memory: 87292 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.swift
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.swift
@@ -1,25 +1,22 @@
 import Foundation
 
-let index = Int(readLine()!)!
-let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
-let targetNum = Int(readLine()!)!
+let n = Int(readLine()!)!
+let nums = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+let x = Int(readLine()!)!
 
-var startIndex = 0
-var lastIndex = index - 1
-var count = 0
+var start = 0
+var end = n - 1
+var answer = 0
 
-while startIndex < lastIndex {
-    let target = numbers[startIndex] + numbers[lastIndex]
-    
-    if target == targetNum {
-        count += 1
-        startIndex += 1
-        lastIndex -= 1
-    } else if target > targetNum {
-        lastIndex -= 1
+while start < end {
+    if nums[start] + nums[end] == x {
+        answer += 1
+        start += 1
+    } else if nums[start] + nums[end] > x {
+        end -= 1
     } else {
-        startIndex += 1
+        start += 1
     }
 }
 
-print(count)
+print(answer)
