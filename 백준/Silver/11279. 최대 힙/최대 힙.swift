e690821687756203fdd76db3695fc85baca20e03
Hano <88874280+Glsme@users.noreply.github.com>
2025-02-10 21:28:12 +0900

    [Silver II] Title: 최대 힙, Time: 104 ms, Memory: 70672 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.swift
@@ -0,0 +1,71 @@
+struct Heap {
+    private var elements: [Int] = []
+    private var comparer: (Int, Int) -> Bool
+    
+    init(comparer: @escaping (Int, Int) -> Bool) {
+        self.comparer = comparer
+    }
+    
+    mutating func insert(_ element: Int) {
+        if elements.isEmpty {
+            elements.append(element)
+            elements.append(element)
+            return
+        }
+        
+        elements.append(element)
+        swimUp(index: elements.count - 1)
+    }
+    
+    mutating func pop() -> Int? {
+        if elements.count < 2 { return nil }
+        elements.swapAt(1, elements.count - 1)
+        let deletedElement = elements.removeLast()
+        diveDown(index: 1)
+        return deletedElement
+    }
+    
+    mutating private func swimUp(index: Int) {
+        var index = index
+        
+        while index > 1, comparer(elements[index], elements[index / 2]) {
+            elements.swapAt(index, index / 2)
+            index /= 2
+        }
+    }
+    
+    mutating private func diveDown(index: Int) {
+        var swapIndex = index
+        var isSwap = false
+        let leftIndex = index * 2
+        let rightIndex = index * 2 + 1
+        
+        if leftIndex < elements.endIndex, comparer(elements[leftIndex], elements[swapIndex]) {
+            swapIndex = leftIndex
+            isSwap = true
+        }
+        
+        if rightIndex < elements.endIndex, comparer(elements[rightIndex], elements[swapIndex]) {
+            swapIndex = rightIndex
+            isSwap = true
+        }
+        
+        if isSwap {
+            elements.swapAt(swapIndex, index)
+            diveDown(index: swapIndex)
+        }
+    }
+}
+
+let count = Int(readLine()!)!
+var heap = Heap(comparer: >)
+
+for _ in 1...count {
+    let num = Int(readLine()!)!
+    
+    if num == 0 {
+        print(heap.pop() ?? 0)
+    } else {
+        heap.insert(num)
+    }
+}
