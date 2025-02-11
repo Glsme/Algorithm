3a8c2fefbc7a8f8c31dc2596d84faa7906539db6
Hano <88874280+Glsme@users.noreply.github.com>
2025-02-11 23:14:57 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let count = Int(readLine()!)!
+let array = readLine()!.split(separator: " ").map { Int($0)! }
+var dp = Array(repeating: 1, count: count)
+
+for i in 0..<count {
+    for j in 0..<i {
+        if array[i] < array[j] {
+            dp[i] = max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+print(dp.max()!)
