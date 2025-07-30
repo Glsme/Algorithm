7d11e626dff66c44acc5d90f3d8251607e048a01
Hano <88874280+Glsme@users.noreply.github.com>
2025-07-30 23:00:24 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 12 ms, Memory: 79692 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.swift
@@ -1,43 +1,17 @@
 import Foundation
 
-struct Queue<T> {
-    private var queue: [T] = []
-    private var index = 0
-    
-    var isEmpty: Bool {
-        return queue.count - index <= 0
-    }
-    
-    mutating func push(_ element: T) {
-        queue.append(element)
-    }
-    
-    @discardableResult
-    mutating func pop() -> T? {
-        guard !self.isEmpty else { return nil }
-        let element = queue[index]
-        index += 1
-        
-        return element
-    }
+let line = readLine()!.split(separator: " ").map { Int($0)! }
+let N = line[0]
+let K = line[1]
+
+var circle = Array(1...N)
+var result = [Int]()
+var index = 0
+
+while !circle.isEmpty {
+    index = (index + K - 1) % circle.count
+    result.append(circle.remove(at: index))
 }
 
-let line = readLine()!.split(separator: " ").map { Int(String($0))! }
-let n = line[0]
-let k = line[1]
-var queue = Queue<Int>()
-var answer: [Int] = []
-
-for num in 1...n {
-    queue.push(num)
-}
-
-while !queue.isEmpty {
-    for _ in 0..<k - 1 {
-        queue.push(queue.pop()!)
-    }
-    
-    answer.append(queue.pop()!)
-}
-
-print("<\(answer.map { String($0) }.joined(separator: ", "))>")
+let output = "<" + result.map { String($0) }.joined(separator: ", ") + ">"
+print(output)
